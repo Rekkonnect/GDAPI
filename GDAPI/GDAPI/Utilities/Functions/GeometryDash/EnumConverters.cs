81792ac59dddd743f1cd6dee9805f6c7a10c1267
AlFas <AlFasGD@users.noreply.github.com>
2019-09-15 03:23:04 +0300

    Add more constants and functions for LevelLength

--- GDAPI/GDAPI/Utilities/Functions/GeometryDash/EnumConverters.cs
+++ GDAPI/GDAPI/Utilities/Functions/GeometryDash/EnumConverters.cs
@@ -9,7 +9,7 @@ using static GDAPI.Utilities.Information.GeometryDash.LevelLengths;
 namespace GDAPI.Utilities.Functions.GeometryDash
 {
     /// <summary>Provides functions to convert values based on level lengths.</summary>
-    public static class LevelLengthConversion
+    public static class EnumConverters
     {
         /// <summary>Returns the <seealso cref="LevelLength"/> of a level from the seconds.</summary>
         /// <param name="seconds">The seconds of the length of the level to get the <seealso cref="LevelLength"/> value of.</param>
@@ -24,6 +24,25 @@ namespace GDAPI.Utilities.Functions.GeometryDash
             if (seconds >= MinSmallLength)
                 return LevelLength.Small;
             return LevelLength.Tiny;
+        }
+        /// <summary>Returns the respective time length range, given a <seealso cref="LevelLength"/>.</summary>
+        /// <param name="length">The <seealso cref="LevelLength"/> whose respective time length range to return.</param>
+        public static Range<double> GetLevelLengthRange(LevelLength length)
+        {
+            switch (length)
+            {
+                case LevelLength.XL:
+                    return XLLengthRange;
+                case LevelLength.Long:
+                    return LongLengthRange;
+                case LevelLength.Medium:
+                    return MediumLengthRange;
+                case LevelLength.Small:
+                    return SmallLengthRange;
+                case LevelLength.Tiny:
+                default:
+                    return TinyLengthRange;
+            }
         }
     }
 }
--- GDAPI/GDAPI/Utilities/Information/GeometryDash/LevelLengths.cs
+++ GDAPI/GDAPI/Utilities/Information/GeometryDash/LevelLengths.cs
@@ -6,17 +6,40 @@ using System.Threading.Tasks;
 
 namespace GDAPI.Utilities.Information.GeometryDash
 {
+    /// <summary>Contains the constants for the lengths</summary>
     public static class LevelLengths
     {
         /// <summary>The minimum time length for the XL level length.</summary>
-        public const int MinXLLength = 120;
+        public const double MinXLLength = 120;
         /// <summary>The minimum time length for the Long level length.</summary>
-        public const int MinLongLength = 60;
+        public const double MinLongLength = 60;
         /// <summary>The minimum time length for the Medium level length.</summary>
-        public const int MinMediumLength = 30;
+        public const double MinMediumLength = 30;
         /// <summary>The minimum time length for the Small level length.</summary>
-        public const int MinSmallLength = 10;
+        public const double MinSmallLength = 10;
         /// <summary>The minimum time length for the Tiny level length.</summary>
-        public const int MinTinyLength = 0;
+        public const double MinTinyLength = 0;
+
+        /// <summary>The maximum time length for the XL level length.</summary>
+        public const double MaxXLLength = double.PositiveInfinity;
+        /// <summary>The maximum time length for the Long level length.</summary>
+        public const double MaxLongLength = 120;
+        /// <summary>The maximum time length for the Medium level length.</summary>
+        public const double MaxMediumLength = 60;
+        /// <summary>The maximum time length for the Small level length.</summary>
+        public const double MaxSmallLength = 30;
+        /// <summary>The maximum time length for the Tiny level length.</summary>
+        public const double MaxTinyLength = 10;
+
+        /// <summary>The time length range for the XL level length.</summary>
+        public static readonly Range<double> XLLengthRange = new Range<double>(MinXLLength, MaxXLLength);
+        /// <summary>The time length range for the Long level length.</summary>
+        public static readonly Range<double> LongLengthRange = new Range<double>(MinLongLength, MaxLongLength);
+        /// <summary>The time length range for the Medium level length.</summary>
+        public static readonly Range<double> MediumLengthRange = new Range<double>(MinMediumLength, MaxMediumLength);
+        /// <summary>The time length range for the Small level length.</summary>
+        public static readonly Range<double> SmallLengthRange = new Range<double>(MinSmallLength, MaxSmallLength);
+        /// <summary>The time length range for the Tiny level length.</summary>
+        public static readonly Range<double> TinyLengthRange = new Range<double>(MinTinyLength, MaxTinyLength);
     }
 }
