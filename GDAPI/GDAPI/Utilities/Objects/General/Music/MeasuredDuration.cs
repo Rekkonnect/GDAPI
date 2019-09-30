f33a7ffd6c7305470a405298ede7e66af76d30ec
AlFas <AlFasGD@users.noreply.github.com>
2019-09-30 13:29:55 +0300

    Improve music structs

--- GDAPI/GDAPI/Utilities/Objects/General/Music/MeasuredDuration.cs
+++ GDAPI/GDAPI/Utilities/Objects/General/Music/MeasuredDuration.cs
@@ -1,17 +1,26 @@
 ﻿using GDAPI.Utilities.Enumerations;
 using System;
+using System.Runtime.InteropServices;
+using static System.Convert;
 
 namespace GDAPI.Utilities.Objects.General.Music
 {
     /// <summary>Represents a measured duration represented a measure, a beat and a fraction of the beat. The struct's size is 8 bytes.</summary>
-    public struct MeasuredDuration
+    [StructLayout(LayoutKind.Explicit)]
+    public struct MeasuredDuration : IComparable<MeasuredDuration>
     {
-        /// <summary>Denotes .</summary>
+        /// <summary>Represents the absolute zero duration.</summary>
         public static readonly MeasuredDuration Zero = new MeasuredDuration(0);
 
+        [FieldOffset(0)]
         private short m;
+        [FieldOffset(2)]
         private ushort b;
+        [FieldOffset(4)]
         private float f;
+
+        [FieldOffset(0)]
+        private ulong all;
 
         /// <summary>The measures of the duration.</summary>
         public int Measures
@@ -37,6 +46,17 @@ namespace GDAPI.Utilities.Objects.General.Music
             }
         }
 
+        /// <summary>Gets or sets the beats along with the beat fraction, without taking the measures into account.</summary>
+        public float BeatsWithFraction
+        {
+            get => Beats + Fraction;
+            set
+            {
+                f = value;
+                FixFraction();
+            }
+        }
+
         /// <summary>Initializes a new instance of the <seealso cref="MeasuredDuration"/> struct.</summary>
         /// <param name="measures">The measures of the duration.</param>
         /// <param name="beats">The beats of the duration.</param>
@@ -54,9 +74,18 @@ namespace GDAPI.Utilities.Objects.General.Music
         /// <param name="fraction">The beat fraction of the duration. It has to be within the range [0, 1).</param>
         public MeasuredDuration(short measures, ushort beats, float fraction)
         {
+            all = 0;
             m = measures;
             b = beats;
             f = fraction;
+        }
+        /// <summary>Initializes a new instance of the <seealso cref="MeasuredDuration"/> struct.</summary>
+        /// <param name="absoluteBeats">The absolute beats.</param>
+        /// <param name="timeSignature">The <seealso cref="TimeSignature"/> based on which to calculate the beats and measures.</param>
+        public MeasuredDuration(float absoluteBeats, TimeSignature timeSignature)
+            : this()
+        {
+            IncreaseFraction(absoluteBeats, timeSignature);
         }
 
         /// <summary>Increases the beat fraction by a value based on the provided <seealso cref="TimeSignature"/>.</summary>
@@ -110,6 +139,22 @@ namespace GDAPI.Utilities.Objects.General.Music
             m = 0;
         }
 
+        /// <summary>Gets the duration as total beats based on a <seealso cref="TimeSignature"/>.</summary>
+        /// <param name="timeSignature">The time signature based on which the number of beats is calculated.</param>
+        public float TotalBeats(TimeSignature timeSignature) => timeSignature.Beats * m + BeatsWithFraction;
+        /// <summary>Gets the duration of this <seealso cref="MeasuredDuration"/> based on a <seealso cref="BPM"/> in seconds based on a <seealso cref="TimeSignature"/>.</summary>
+        /// <param name="bpm">The <seealso cref="BPM"/> that determines the duration.</param>
+        /// <param name="timeSignature">The <seealso cref="TimeSignature"/> that determines the measure's beat count.</param>
+        public double GetDuration(BPM bpm, TimeSignature timeSignature) => bpm.GetDuration(this, timeSignature);
+        /// <summary>Gets the duration of this <seealso cref="MeasuredDuration"/> based on a <seealso cref="BPM"/> as a <seealso cref="TimeSpan"/> based on a <seealso cref="TimeSignature"/>.</summary>
+        /// <param name="bpm">The <seealso cref="BPM"/> that determines the duration.</param>
+        /// <param name="timeSignature">The <seealso cref="TimeSignature"/> that determines the measure's beat count.</param>
+        public TimeSpan GetDurationTimeSpan(BPM bpm, TimeSignature timeSignature) => bpm.GetDurationTimeSpan(this, timeSignature);
+
+        /// <summary>Compares this <seealso cref="MeasuredDuration"/> with another based on the absolute value.</summary>
+        /// <param name="other">The other <seealso cref="MeasuredDuration"/> to compare this to.</param>
+        public int CompareTo(MeasuredDuration other) => CompareByAbsoluteValue(this, other);
+
         private void FixFraction()
         {
             b += (ushort)f;
@@ -125,5 +170,50 @@ namespace GDAPI.Utilities.Objects.General.Music
             m += (short)((b - 1) / timeSignature.Beats);
             b = (ushort)((b - 1) % timeSignature.Beats + 1);
         }
+
+        public static bool operator ==(MeasuredDuration left, MeasuredDuration right) => left.all == right.all;
+        public static bool operator !=(MeasuredDuration left, MeasuredDuration right) => left.all != right.all;
+
+        /// <summary>Parses a string of the form {Measures}:{Beats}.{Fraction} into a <seealso cref="MeasuredDuration"/>.</summary>
+        /// <param name="s">The string to parse into a <seealso cref="MeasuredDuration"/>.</param>
+        public unsafe static MeasuredDuration Parse(string s)
+        {
+            var split = s.Split(':');
+            int measures = ToInt32(split[0]);
+            float fraction = ToSingle(split[1]);
+            int beats = (int)fraction;
+            fraction -= beats;
+            return new MeasuredDuration(measures, beats, fraction);
+        }
+        /// <summary>Attempts to parse a string into a <seealso cref="MeasuredDuration"/>. Returns a <seealso cref="bool"/> determining whether the operation succeeded.</summary>
+        /// <param name="s">The string to parse into a <seealso cref="MeasuredDuration"/>.</param>
+        /// <param name="duration">The <seealso cref="MeasuredDuration"/> that will be parsed. If the string is unparsable, the value is <see langword="default"/>.</param>
+        public static bool TryParse(string s, out MeasuredDuration duration)
+        {
+            duration = default;
+            var split = s.Split(':');
+            if (!int.TryParse(split[0], out int measures))
+                return false;
+            if (!float.TryParse(split[1], out float fraction))
+                return false;
+            int beats = (int)fraction;
+            fraction -= beats;
+            duration = new MeasuredDuration(measures, beats, fraction);
+            return true;
+        }
+
+        /// <summary>Defines a <seealso cref="Comparison{T}"/> to compare between <seealso cref="MeasuredDuration"/> based on their measures. Returns the <seealso cref="int"/> value of <seealso cref="IComparable{T}.CompareTo(T)"/>.</summary>
+        /// <param name="left">The left <seealso cref="MeasuredDuration"/> to compare.</param>
+        /// <param name="right">The right <seealso cref="MeasuredDuration"/> to compare.</param>
+        public static int CompareByMeasure(MeasuredDuration left, MeasuredDuration right) => left.m.CompareTo(right.m);
+        /// <summary>Defines a <seealso cref="Comparison{T}"/> to compare between <seealso cref="MeasuredDuration"/> based on their absolute value. That means the one with the larger measures, then beats, then fraction is considered larger. Returns the <seealso cref="int"/> value of <seealso cref="IComparable{T}.CompareTo(T)"/>.</summary>
+        /// <param name="left">The left <seealso cref="MeasuredDuration"/> to compare.</param>
+        /// <param name="right">The right <seealso cref="MeasuredDuration"/> to compare.</param>
+        public static unsafe int CompareByAbsoluteValue(MeasuredDuration left, MeasuredDuration right) => left.all.CompareTo(right.all);
+
+        /// <summary>Returns the string representation of this <seealso cref="MeasuredDuration"/> of the form {Measures}:{Beats}.{Fraction}.</summary>
+        public override string ToString() => $"{m}:{b}.{DecimalPartOf(f.ToString("F3"))}";
+
+        private static string DecimalPartOf(string s) => s.Substring(s.IndexOf('.') + 1);
     }
 }
--- GDAPI/GDAPI/Utilities/Objects/General/Music/MeasuredTimePosition.cs
+++ GDAPI/GDAPI/Utilities/Objects/General/Music/MeasuredTimePosition.cs
@@ -1,17 +1,28 @@
 ﻿using GDAPI.Utilities.Enumerations;
 using System;
+using System.Runtime.InteropServices;
+using static System.Convert;
 
 namespace GDAPI.Utilities.Objects.General.Music
 {
     /// <summary>Represents a time position that contains a measure, a beat and a fraction of the beat. The struct's size is 8 bytes.</summary>
-    public struct MeasuredTimePosition
+    [StructLayout(LayoutKind.Explicit)]
+    public struct MeasuredTimePosition : IComparable<MeasuredTimePosition>
     {
         /// <summary>Denotes the starting position of a musical composition.</summary>
         public static readonly MeasuredTimePosition Start = new MeasuredTimePosition(1, 1, 0);
+        /// <summary>A standarized constant that denotes the ending position of a musical composition, without knowing its length.</summary>
+        public static readonly MeasuredTimePosition UnknownEnd = new MeasuredTimePosition(short.MinValue, (ushort)0, 0);
 
+        [FieldOffset(0)]
         private short m;
+        [FieldOffset(2)]
         private ushort b;
+        [FieldOffset(4)]
         private float f;
+
+        [FieldOffset(0)]
+        private ulong all;
 
         /// <summary>The 1-indexed measure of the time position.</summary>
         public int Measure
@@ -37,6 +48,26 @@ namespace GDAPI.Utilities.Objects.General.Music
             }
         }
 
+        /// <summary>Gets or sets the beat along with the beat fraction, without taking the measures into account.</summary>
+        public float BeatWithFraction
+        {
+            get => Beat + Fraction;
+            set
+            {
+                f = value;
+                FixFraction();
+            }
+        }
+
+        /// <summary>Determines whether the time position is on the start of the measure.</summary>
+        public bool IsStartOfMeasure
+        {
+            get => b == 1 && f == 0;
+            set => ResetToMeasureStart();
+        }
+        /// <summary>Determines whether this is equal to the standarized constant <seealso cref="UnknownEnd"/>.</summary>
+        public bool IsUnknownEnd => this == UnknownEnd;
+
         /// <summary>Initializes a new instance of the <seealso cref="MeasuredTimePosition"/> struct.</summary>
         /// <param name="measure">The measure of the time position.</param>
         /// <param name="beat">The beat of the time position.</param>
@@ -59,6 +90,7 @@ namespace GDAPI.Utilities.Objects.General.Music
         /// <param name="fraction">The beat fraction of the time position. It has to be within the range [0, 1).</param>
         public MeasuredTimePosition(short measure, ushort beat, float fraction)
         {
+            all = 0;
             m = measure;
             b = beat;
             f = fraction;
@@ -90,15 +122,31 @@ namespace GDAPI.Utilities.Objects.General.Music
         /// <summary>Advances the measure by a value.</summary>
         /// <param name="measures">The number of measures to advance by.</param>
         public void AdvanceMeasure(int measures) => m += (short)measures;
+        /// <summary>Advances the measure by one and resets the position to the start of the measure.</summary>
+        public void AdvanceToStartOfNextMeasure()
+        {
+            AdvanceMeasure();
+            ResetToMeasureStart();
+        }
+        /// <summary>Advances to the start of the next measure if the current position is beyond the start of the current measure.</summary>
+        public void ResetMeasureIfBeyondStart()
+        {
+            if (!IsStartOfMeasure)
+                AdvanceToStartOfNextMeasure();
+        }
 
         /// <summary>Advances this time position by a <seealso cref="MusicalNoteValue"/> based on the provided <seealso cref="TimeSignature"/>.</summary>
         /// <param name="value">The value to advance this time position by.</param>
         /// <param name="timeSignature">The <seealso cref="TimeSignature"/> based on which to advance the beat.</param>
         public void AdvanceValue(MusicalNoteValue value, TimeSignature timeSignature) => AdvanceFraction((float)value / timeSignature.Denominator, timeSignature);
-        /// <summary>Advances this time position by a <seealso cref="MusicalNoteValue"/> based on the provided <seealso cref="TimeSignature"/>.</summary>
+        /// <summary>Advances this time position by a <seealso cref="RhythmicalValue"/> based on the provided <seealso cref="TimeSignature"/>.</summary>
         /// <param name="value">The value to advance this time position by.</param>
         /// <param name="timeSignature">The <seealso cref="TimeSignature"/> based on which to advance the beat.</param>
         public void AdvanceValue(RhythmicalValue value, TimeSignature timeSignature) => AdvanceFraction((float)value.TotalValue * timeSignature.Denominator, timeSignature);
+        /// <summary>Advances this time position by a <seealso cref="MeasuredDuration"/> based on the provided <seealso cref="TimeSignature"/>.</summary>
+        /// <param name="value">The value to advance this time position by.</param>
+        /// <param name="timeSignature">The <seealso cref="TimeSignature"/> based on which to advance the beat.</param>
+        public void AdvanceValue(MeasuredDuration value, TimeSignature timeSignature) => AdvanceFraction(value.BeatsWithFraction, timeSignature);
 
         /// <summary>Resets the beat fraction to 0, which is the start of the beat.</summary>
         public void ResetToBeatStart() => f = 0;
@@ -115,6 +163,25 @@ namespace GDAPI.Utilities.Objects.General.Music
             m = 1;
         }
 
+        /// <summary>Gets this <seealso cref="MeasuredTimePosition"/>'s absolute beat based on a <seealso cref="TimeSignature"/>, which is calculated based on the measure.</summary>
+        /// <param name="timeSignature">The <seealso cref="TimeSignature"/> based on which to calculate the absolute beat.</param>
+        public float GetAbsoluteBeat(TimeSignature timeSignature) => timeSignature.Beats * (m - 1) + b + f;
+
+        /// <summary>Gets the distance from another <seealso cref="MeasuredTimePosition"/> based on a <seealso cref="TimeSignature"/> and returns a <seealso cref="MeasuredDuration"/> representing that distance.</summary>
+        /// <param name="other">The other <seealso cref="MeasuredTimePosition"/> to get the distance from.</param>
+        /// <param name="timeSignature">The <seealso cref="TimeSignature"/> based on which to calculate the distance.</param>
+        public MeasuredDuration DistanceFrom(MeasuredTimePosition other, TimeSignature timeSignature)
+        {
+            float thisBeat = GetAbsoluteBeat(timeSignature);
+            float otherBeat = other.GetAbsoluteBeat(timeSignature);
+
+            return new MeasuredDuration(Math.Abs(otherBeat - thisBeat), timeSignature);
+        }
+
+        /// <summary>Compares this <seealso cref="MeasuredTimePosition"/> with another based on the absolute position.</summary>
+        /// <param name="other">The other <seealso cref="MeasuredTimePosition"/> to compare this to.</param>
+        public int CompareTo(MeasuredTimePosition other) => CompareByAbsolutePosition(this, other);
+
         private void FixFraction()
         {
             b += (ushort)f;
@@ -130,5 +197,58 @@ namespace GDAPI.Utilities.Objects.General.Music
             m += (short)((b - 1) / timeSignature.Beats);
             b = (ushort)((b - 1) % timeSignature.Beats + 1);
         }
+
+        public static bool operator ==(MeasuredTimePosition left, MeasuredTimePosition right) => left.all == right.all;
+        public static bool operator !=(MeasuredTimePosition left, MeasuredTimePosition right) => left.all == right.all;
+
+        /// <summary>Parses a string of the form {Measure}:{Beat}.{Fraction} into a <seealso cref="MeasuredTimePosition"/>.</summary>
+        /// <param name="s">The string to parse into a <seealso cref="MeasuredTimePosition"/>.</param>
+        public static MeasuredTimePosition Parse(string s)
+        {
+            var split = s.Split(':');
+            int measure = ToInt32(split[0]);
+            float fraction = ToSingle(split[1]);
+            int beat = (int)fraction;
+            fraction -= beat;
+            return new MeasuredTimePosition(measure, beat, fraction);
+        }
+        /// <summary>Attempts to parse a string into a <seealso cref="MeasuredTimePosition"/>. Returns a <seealso cref="bool"/> determining whether the operation succeeded.</summary>
+        /// <param name="s">The string to parse into a <seealso cref="MeasuredTimePosition"/>.</param>
+        /// <param name="timePosition">The <seealso cref="MeasuredTimePosition"/> that will be parsed. If the string is unparsable, the value is <see langword="default"/>.</param>
+        public static bool TryParse(string s, out MeasuredTimePosition timePosition)
+        {
+            timePosition = default;
+            var split = s.Split(':');
+            if (!int.TryParse(split[0], out int measure))
+                return false;
+            if (!float.TryParse(split[1], out float fraction))
+                return false;
+            int beat = (int)fraction;
+            fraction -= beat;
+            timePosition = new MeasuredTimePosition(measure, beat, fraction);
+            return true;
+        }
+        /// <summary>Parses a string of the form {Beat}.{Fraction} into a <seealso cref="MeasuredTimePosition"/>.</summary>
+        /// <param name="s">The string to parse into a <seealso cref="MeasuredTimePosition"/>.</param>
+        public static MeasuredTimePosition ParseAsBeatWithFraction(string s)
+        {
+            var result = new MeasuredTimePosition();
+            result.BeatWithFraction = float.Parse(s);
+            return result;
+        }
+
+        /// <summary>Defines a <seealso cref="Comparison{T}"/> to compare between <seealso cref="MeasuredTimePosition"/> based on their measures. Returns the <seealso cref="int"/> value of <seealso cref="IComparable{T}.CompareTo(T)"/>.</summary>
+        /// <param name="left">The left <seealso cref="MeasuredTimePosition"/> to compare.</param>
+        /// <param name="right">The right <seealso cref="MeasuredTimePosition"/> to compare.</param>
+        public static int CompareByMeasure(MeasuredTimePosition left, MeasuredTimePosition right) => left.m.CompareTo(right.m);
+        /// <summary>Defines a <seealso cref="Comparison{T}"/> to compare between <seealso cref="MeasuredTimePosition"/> based on their absolute position. That means the one with the larger measure, then beat, then fraction is considered larger. Returns the <seealso cref="int"/> value of <seealso cref="IComparable{T}.CompareTo(T)"/>.</summary>
+        /// <param name="left">The left <seealso cref="MeasuredTimePosition"/> to compare.</param>
+        /// <param name="right">The right <seealso cref="MeasuredTimePosition"/> to compare.</param>
+        public static unsafe int CompareByAbsolutePosition(MeasuredTimePosition left, MeasuredTimePosition right) => left.all.CompareTo(right.all);
+
+        /// <summary>Returns the string representation of this <seealso cref="MeasuredTimePosition"/> of the form {Measure}:{Beat}.{Fraction}.</summary>
+        public override string ToString() => $"{m}:{b}.{DecimalPartOf(f.ToString("F3"))}";
+
+        private static string DecimalPartOf(string s) => s.Substring(s.IndexOf('.') + 1);
     }
 }
