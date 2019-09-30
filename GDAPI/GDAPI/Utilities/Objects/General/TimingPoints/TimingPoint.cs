87633bac16e4c82b85a45695fba8afdd13eebdaa
AlFas <AlFasGD@users.noreply.github.com>
2019-09-30 13:37:13 +0300

    Add timing points

---
+++ GDAPI/GDAPI/Utilities/Objects/General/TimingPoints/TimingPoint.cs
@@ -0,0 +1,78 @@
+﻿using GDAPI.Utilities.Objects.General.Music;
+using System;
+
+namespace GDAPI.Utilities.Objects.General.TimingPoints
+{
+    /// <summary>Represents a timing point in a <seealso cref="GuidelineEditorPreset"/>.</summary>
+    public abstract class TimingPoint : IComparable<TimingPoint>
+    {
+        /// <summary>The BPM of the timing point.</summary>
+        public BPM BPM;
+        /// <summary>The time signature of the timing point.</summary>
+        public TimeSignature TimeSignature;
+
+        /// <summary>Initializes a new instance of the <seealso cref="TimingPoint"/> class.</summary>
+        /// <param name="bpm">The BPM of the timing point.</param>
+        /// <param name="timeSignature">The time signature of the timing point.</param>
+        public TimingPoint(BPM bpm, TimeSignature timeSignature)
+        {
+            BPM = bpm;
+            TimeSignature = timeSignature;
+        }
+
+        /// <summary>Attempts to parse a string representation of a timing point into a <seealso cref="TimingPoint"/> and returns the resulting parsed object, or <see langword="null"/>.</summary>
+        /// <param name="s">The string representation of a timing point to parse.</param>
+        public static TimingPoint Parse(string s)
+        {
+            if (AbsoluteTimingPoint.TryParse(s, out var absolute))
+                return absolute;
+            if (RelativeTimingPoint.TryParse(s, out var relative))
+                return relative;
+            return null;
+        }
+
+        /// <summary>Compares this <seealso cref="TimingPoint"/>'s time position with another.</summary>
+        /// <param name="other">The other <seealso cref="TimingPoint"/> whose time position to compare this one's to.</param>
+        public int CompareTo(TimingPoint other) => CompareTimePosition(other);
+
+        /// <summary>Returns the absolute time position of this timing point as a <seealso cref="TimeSpan"/>.</summary>
+        public abstract TimeSpan GetAbsoluteTimePosition();
+        /// <summary>Returns the relative time position of this timing point as a <seealso cref="MeasuredTimePosition"/>.</summary>
+        public abstract MeasuredTimePosition GetRelativeTimePosition();
+        /// <summary>Calculates the relative or absolute time position of this timing poing based on a previous <seealso cref="TimingPoint"/>, depending on which is not known.</summary>
+        /// <param name="previous">The previous <seealso cref="TimingPoint"/> based on which to calculate the relative or absolute time position.</param>
+        public abstract void CalculateTimePosition(TimingPoint previous);
+
+        /// <summary>Compares the time position of this and another timing point's. It is defined so that inherited classes handle comparison.</summary>
+        /// <param name="other">The other timing point to compare to.</param>
+        protected abstract int CompareTimePosition(TimingPoint other);
+    }
+    /// <summary>Represents a timing point in a <seealso cref="GuidelineEditorPreset"/>.</summary>
+    /// <typeparam name="T">The type of the time position.</typeparam>
+    public abstract class TimingPoint<T> : TimingPoint, IComparable<TimingPoint<T>>
+        where T : struct, IComparable<T>
+    {
+        /// <summary>The time position of the event.</summary>
+        public T TimePosition;
+
+        /// <summary>Initializes a new instance of the <seealso cref="TimingPoint{T}"/> class.</summary>
+        /// <param name="timePosition">The time position of the event.</param>
+        /// <param name="bpm">The BPM of the timing point.</param>
+        /// <param name="timeSignature">The time signature of the timing point.</param>
+        public TimingPoint(T timePosition, BPM bpm, TimeSignature timeSignature)
+            : base(bpm, timeSignature)
+        {
+            TimePosition = timePosition;
+        }
+
+        /// <summary>Compares this <seealso cref="TimingPoint"/>'s time position with another.</summary>
+        /// <param name="other">The other <seealso cref="TimingPoint"/> whose time position to compare this one's to.</param>
+        public int CompareTo(TimingPoint<T> other) => TimePosition.CompareTo(other.TimePosition);
+
+        /// <inheritdoc/>
+        protected sealed override int CompareTimePosition(TimingPoint other) => CompareTo(other as TimingPoint<T>);
+
+        /// <summary>Returns the string representation of this timing point.</summary>
+        public override string ToString() => $"{TimePosition}|{BPM}|{TimeSignature}";
+    }
+}
---
+++ GDAPI/GDAPI/Utilities/Objects/General/TimingPoints/TimingPointList.cs
@@ -0,0 +1,136 @@
+﻿using GDAPI.Utilities.Functions.Extensions;
+using GDAPI.Utilities.Objects.General.DataStructures;
+using GDAPI.Utilities.Objects.General.Music;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GDAPI.Utilities.Objects.General.TimingPoints
+{
+    /// <summary>Contains information about an event track in a <seealso cref="GuidelineEditorPresetPattern"/>.</summary>
+    public class TimingPointList
+    {
+        private SortedList<TimingPoint> timingPoints;
+
+        /// <summary>Creates a new empty instance of the <seealso cref="TimingPointList"/> class.</summary>
+        public TimingPointList() { }
+        /// <summary>Creates a new instance of the <seealso cref="TimingPointList"/> class.</summary>
+        /// <param name="presetTimingPoints">The list of tracks that are contained.</param>
+        public TimingPointList(List<TimingPoint> presetTimingPoints)
+        {
+            timingPoints = ConvertToSortedList(presetTimingPoints);
+        }
+        /// <summary>Creates a new instance of the <seealso cref="TimingPointList"/> class.</summary>
+        /// <param name="presetTimingPoints">The list of tracks that are contained.</param>
+        public TimingPointList(SortedList<TimingPoint> presetTimingPoints)
+        {
+            timingPoints = new SortedList<TimingPoint>(presetTimingPoints);
+        }
+
+        /// <summary>Adds a <seealso cref="TimingPoint"/> to the list.</summary>
+        /// <param name="timingPoint">The <seealso cref="TimingPoint"/> to add to the list.</param>
+        public void Add(TimingPoint timingPoint) => RecalculateTimePositions(timingPoints.Insert(timingPoint) + 1);
+        /// <summary>Adds a collection of <seealso cref="TimingPoint"/>s to the list.</summary>
+        /// <param name="points">The <seealso cref="TimingPoint"/>s to add to the list.</param>
+        public void AddRange(IEnumerable<TimingPoint> points)
+        {
+            if (!points.Any())
+                return;
+
+            int minIndex = int.MaxValue;
+            foreach (var p in points)
+            {
+                int index = timingPoints.Insert(p) + 1;
+                if (index < minIndex)
+                    minIndex = index;
+            }
+            RecalculateTimePositions(minIndex);
+        }
+        /// <summary>Removes a <seealso cref="TimingPoint"/> from the list.</summary>
+        /// <param name="timingPoint">The <seealso cref="TimingPoint"/> to remove from the list.</param>
+        public void Remove(TimingPoint timingPoint)
+        {
+            if (timingPoints.RemoveIfNotAtIndex(timingPoint, 0, out int index))
+                RecalculateTimePositions(index);
+        }
+
+        /// <summary>Clones this instance and returns the new instance.</summary>
+        public TimingPointList Clone() => new TimingPointList(timingPoints);
+
+        private void RecalculateTimePositions(int firstIndex)
+        {
+            for (int i = firstIndex; i < timingPoints.Count; i++)
+                timingPoints[i].CalculateTimePosition(timingPoints[i - 1]);
+        }
+
+        /// <summary>Gets the string representation of this <seealso cref="GuidelineEditorPresetTrackList"/> that will be used in the preset data.</summary>
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            foreach (var p in timingPoints)
+                result.AppendLine(p.ToString()).AppendLine();
+
+            return result.RemoveLastIfEndsWith('\n').ToString();
+        }
+
+        private static SortedList<TimingPoint> ConvertToSortedList(List<TimingPoint> presetTimingPoints)
+        {
+            var timingPoints = new SortedList<TimingPoint>(presetTimingPoints.Count, CompareTimingPoints);
+
+            var absoluteTimingPoints = Convert<TimingPoint, AbsoluteTimingPoint>(presetTimingPoints);
+
+            if (absoluteTimingPoints.Count == 0)
+                throw new InvalidOperationException("No absolute timing points were used");
+            absoluteTimingPoints[0].SetAsInitialTimingPoint();
+
+            var relativeTimingPoints = Convert<TimingPoint, RelativeTimingPoint>(presetTimingPoints);
+            relativeTimingPoints.Sort((a, b) => MeasuredTimePosition.CompareByAbsolutePosition(a.TimePosition, b.TimePosition));
+
+            // Add absolute timing points and calculate their relative time positions
+            AbsoluteTimingPoint previousAbsolute = null;
+            foreach (var a in absoluteTimingPoints)
+            {
+                if (previousAbsolute != null)
+                    a.CalculateRelativeTimePosition(previousAbsolute);
+                timingPoints.Add(previousAbsolute = a);
+            }
+
+            // Add relative timing points, calculate their absolute time positions and adjust the absolute timing points' relative time positions
+            foreach (var r in relativeTimingPoints)
+            {
+                int index = timingPoints.IndexBefore(r);
+                r.CalculateAbsoluteTimePosition(timingPoints[index]);
+                if (index + 1 < timingPoints.Count)
+                {
+                    // The next timing point is certainly an absolute timing point since no relative timing points have been added beyond that one yet
+                    var nextAbsolute = timingPoints[index + 1] as AbsoluteTimingPoint;
+
+                    // Calculate the measure adjustment for the first absolute timing point to apply it to all the rest
+                    int currentMeasure = nextAbsolute.RelativeTimePosition.Measure;
+                    nextAbsolute.CalculateRelativeTimePosition(r);
+                    int newMeasure = nextAbsolute.RelativeTimePosition.Measure;
+                    int measureAdjustment = newMeasure - currentMeasure;
+
+                    if (measureAdjustment != 0)
+                        for (int i = index + 2; i < timingPoints.Count; i++)
+                            (timingPoints[i] as AbsoluteTimingPoint).AdjustMeasure(measureAdjustment);
+                }
+                timingPoints.Add(r);
+            }
+
+            return timingPoints;
+        }
+
+        private static List<TConverted> Convert<TBase, TConverted>(List<TBase> list)
+            where TConverted : class
+        {
+            var result = list.ConvertAll(p => p as TConverted);
+            result.RemoveAll(p => p is null);
+            return result;
+        }
+
+        private static int CompareTimingPoints(TimingPoint left, TimingPoint right) => left.GetRelativeTimePosition().CompareTo(right.GetRelativeTimePosition());
+    }
+}
