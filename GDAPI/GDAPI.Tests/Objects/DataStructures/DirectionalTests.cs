c298dca1d71d84bda17092f8f4574b4e73ec82d9
AlFas <AlFasGD@users.noreply.github.com>
2019-10-14 06:16:42 +0300

    Add Directional tests and fix indexing issue

---
+++ GDAPI/GDAPI.Tests/Objects/DataStructures/DirectionalTests.cs
@@ -0,0 +1,34 @@
+﻿using GDAPI.Objects.DataStructures;
+using NUnit.Framework;
+
+namespace GDAPI.Tests.Objects.DataStructures
+{
+    public class DirectionalTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void DirectionalStuff()
+        {
+            int[,] values = new int[,]
+            {
+                { 0, 1, 2 },
+                { 3, 4, 5 },
+                { 6, 7, 8 },
+            };
+            var d = new Directional<int>(values);
+            Assert.AreEqual(0, d.TopLeft);
+            Assert.AreEqual(1, d.TopCenter);
+            Assert.AreEqual(2, d.TopRight);
+            Assert.AreEqual(3, d.MiddleLeft);
+            Assert.AreEqual(4, d.Center);
+            Assert.AreEqual(5, d.MiddleRight);
+            Assert.AreEqual(6, d.BottomLeft);
+            Assert.AreEqual(7, d.BottomCenter);
+            Assert.AreEqual(8, d.BottomRight);
+        }
+    }
+}
