ffb7a96d7240db83519026bdfdb5f4046ae9bbb7
AlFas <AlFasGD@users.noreply.github.com>
2019-10-14 06:16:30 +0300

    Add bit array tests

---
+++ GDAPI/GDAPI.Tests/Objects/DataStructures/BitArray16Tests.cs
@@ -0,0 +1,44 @@
+﻿using GDAPI.Objects.DataStructures;
+using NUnit.Framework;
+
+namespace GDAPI.Tests.Objects.DataStructures
+{
+    public class BitArray16Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BitArray()
+        {
+            var falseArray = new BitArray16(false);
+            for (int i = 0; i < 16; i++)
+            {
+                Assert.IsFalse(falseArray.GetBoolBit(i));
+                Assert.AreEqual(0, falseArray.GetBit(i));
+            }
+
+            var trueArray = new BitArray16(true);
+            for (int i = 0; i < 16; i++)
+            {
+                Assert.IsTrue(trueArray.GetBoolBit(i));
+                Assert.AreEqual(1, trueArray.GetBit(i));
+            }
+
+            // Do not you fucking dare set a bit to anything other than 0 or 1, the entire thing will break
+            // Just like your heart when you see your data being fucked up, so watch out kid
+
+            var array = new BitArray16(false);
+            array.SetBit(12, 1);
+            Assert.AreEqual(1, array.GetBit(12));
+            array.SetBoolBit(12, false);
+            Assert.AreEqual(0, array.GetBit(12));
+            array[12] = true;
+            Assert.IsTrue(array[12]);
+            array[12] = false;
+            Assert.IsFalse(array[12]);
+        }
+    }
+}
---
+++ GDAPI/GDAPI.Tests/Objects/DataStructures/BitArray8Tests.cs
@@ -0,0 +1,44 @@
+﻿using GDAPI.Objects.DataStructures;
+using NUnit.Framework;
+
+namespace GDAPI.Tests.Objects.DataStructures
+{
+    public class BitArray8Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BitArray()
+        {
+            var falseArray = new BitArray8(false);
+            for (int i = 0; i < 8; i++)
+            {
+                Assert.IsFalse(falseArray.GetBoolBit(i));
+                Assert.AreEqual(0, falseArray.GetBit(i));
+            }
+
+            var trueArray = new BitArray8(true);
+            for (int i = 0; i < 8; i++)
+            {
+                Assert.IsTrue(trueArray.GetBoolBit(i));
+                Assert.AreEqual(1, trueArray.GetBit(i));
+            }
+
+            // Do not you fucking dare set a bit to anything other than 0 or 1, the entire thing will break
+            // Just like your heart when you see your data being fucked up, so watch out kid
+
+            var array = new BitArray8(false);
+            array.SetBit(2, 1);
+            Assert.AreEqual(1, array.GetBit(2));
+            array.SetBoolBit(2, false);
+            Assert.AreEqual(0, array.GetBit(2));
+            array[2] = true;
+            Assert.IsTrue(array[2]);
+            array[2] = false;
+            Assert.IsFalse(array[2]);
+        }
+    }
+}
