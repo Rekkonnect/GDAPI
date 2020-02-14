d30f4913433781eacdaa29a4ad2924032feb32de
Alten <35349837+Altenhh@users.noreply.github.com>
2020-02-14 18:29:38 -0500

    Add back dependencies

--- GDAPI/GDAPI/Program.cs
+++ GDAPI/GDAPI/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace GDAPI
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDAPI
 {
     public static class Program
     {
