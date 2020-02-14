ab338f142d46f80323f9293d0a4541ab2fd538de
Syudagye <syudagye@gmail.com>
2020-02-14 22:18:20 +0100

    added SonMetaGetter and SongDownloader

---
+++ GDAPI/GDAPI/Application/Newgrounds/SongDownloader.cs
@@ -0,0 +1,63 @@
+﻿using GDAPI.Functions.General;
+using GDAPI.Objects.GeometryDash.General;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Management.Automation;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GDAPI.Application.Newgrounds
+{
+    class SongDownloader
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        private Task download;
+
+        public TaskStatus DownloadStatus => download?.Status ?? (TaskStatus)(-1);
+
+        /// <summary>
+        /// Download the matching song from Newgrounds specified by the <seealso cref="SongMetadata"/>
+        /// </summary>
+        /// <param name="song">The song to download</param>
+        public SongDownloader(SongMetadata song)
+        {
+            Task.Run(() => download = DownloadSong(song));
+        }
+
+        /// <summary>
+        /// Download the matching song from Newgrounds specified by the <paramref name="id"/>
+        /// </summary>
+        /// <param name="id">The id of the song to download</param>
+        public SongDownloader(string id)
+        {
+            Task.Run(() =>
+            {
+                var getter = new SongMetaGetter(id);
+                while (true)
+                {
+                    if (getter.Status >= TaskStatus.RanToCompletion)
+                    {
+                        Task.Run(() => download = DownloadSong(getter.Result));
+                        break;
+                    }
+                }
+            });
+        }
+
+        private async Task DownloadSong(SongMetadata song)
+        {
+            if (song == SongMetadata.Unknown)
+                return;
+            else
+            {
+                var filepath = $"{Database.GDLocalData}{Path.DirectorySeparatorChar}{song.ID}.mp3";
+                var res = await client.GetAsync(song.DownloadLink);
+                var bytes = await res.Content.ReadAsByteArrayAsync();
+                File.WriteAllBytes(filepath, bytes);
+            }
+        }
+    }
+}
--- GDAPI/GDAPI/Functions/General/UsefulFunctions.cs
+++ GDAPI/GDAPI/Functions/General/UsefulFunctions.cs
@@ -38,5 +38,39 @@ namespace GDAPI.Functions.General
             result.Remove(result.Length - 1, 1);
             WriteAllText(path, result.ToString());
         }
+        /// <summary>Used to convert specifics hexadecimal chars on a given string (<paramref name="str"/>)</summary>
+        /// <param name="str">The string to process</param>
+        /// <returns>The proper string</returns>
+        public static string ConvertStringWithHexCharsToProperString(string str)
+        {
+            var chars = str.ToCharArray();
+            var result = new StringBuilder();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '%')
+                {
+                    switch (chars[i + 1].ToString() + chars[i + 2].ToString()) // This website for hex codes => https://www.obkb.com/dcljr/charstxt.html
+                    {
+                        case "3A": // colon (not the fox lol)
+                            result.Append(':');
+                            break;
+                        case "2F": // slash
+                            result.Append('/');
+                            break;
+                        case "3F": // Question mark
+                            result.Append('?');
+                            break;
+                        default:
+                            break;
+                    }
+                    i += 2;
+                }
+                else
+                {
+                    result.Append(chars[i]);
+                }
+            }
+            return result.ToString();
+        }
     }
 }
