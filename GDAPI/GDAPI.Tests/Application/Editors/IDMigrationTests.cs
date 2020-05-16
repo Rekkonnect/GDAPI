8d3763692c7e287a8ffc8fce76f616455293ba74
AlFas <AlFasGD@users.noreply.github.com>
2020-05-17 01:25:37 +0300

    Add more tests

--- GDAPI/GDAPI.Tests/Application/Editors/IDMigrationTests.cs
+++ GDAPI/GDAPI.Tests/Application/Editors/IDMigrationTests.cs
@@ -23,8 +23,39 @@ namespace GDAPI.Tests.Application.Editors
         private InstantCountTrigger[] instantCountTriggers = new InstantCountTrigger[5];
         private CollisionBlock[] collisionBlocks = new CollisionBlock[5];
         private CollisionTrigger[] collisionTriggers = new CollisionTrigger[5];
-        private LevelObjectCollection allObjects;
 
+        private LevelObjectCollection allObjects;
+        private Level level;
+        private Editor editor;
+
+        [SetUp]
+        public void Initialize()
+        {
+            InitializeLevelEditorInstances();
+        }
+
+        private void InitializeLevelEditorInstances()
+        {
+            level = new Level();
+            editor = new Editor(level);
+            ReinitializeLevelObjects();
+        }
+
+        private void ReinitializeLevelStuff()
+        {
+            ReinitializeLevelObjects();
+            InitializeColorChannels();
+        }
+        private void ReinitializeLevelObjects()
+        {
+            InitializeObjects();
+            level.LevelObjects = allObjects;
+        }
+        private void InitializeColorChannels()
+        {
+            for (int i = 1; i < 1000; i++)
+                level.ColorChannels[i].CopiedColorID = i; // an indicator for the color channels
+        }
         private void InitializeObjects()
         {
             for (int i = 0; i < 5; i++)
@@ -61,26 +92,18 @@ namespace GDAPI.Tests.Application.Editors
             allObjects = new LevelObjectCollection(normalBlocks.Concat(moveTriggers).Concat(pickupItems).Concat(pickupTriggers).Concat(colorTriggers).Concat(instantCountTriggers).Concat(collisionBlocks).Concat(collisionTriggers));
         }
 
-        #region IDMigration
+        #region ID Migration
         [Test]
         public void IDMigration()
         {
-            InitializeObjects();
-
-            var level = new Level
-            {
-                Name = "ID Migration Test Level",
-                Description = "This is a test level to help testing the ID migration feature in the backend",
-                LevelObjects = allObjects,
-            };
-
-            var editor = new Editor(level);
-
             var step0 = new SourceTargetRange(1, 5, 6);    //  1 -  5 >  6 - 10
             var step1 = new SourceTargetRange(21, 25, 46); // 21 - 25 > 46 - 50
             var step2 = new SourceTargetRange(1, 25, 26);  //  1 - 25 > 26 - 50
             var step3 = new SourceTargetRange(8, 8, 10);   //       8 >      10
 
+            ReinitializeLevelStuff();
+
+            // TODO: Move assertions to within the verification functions
             editor.PerformGroupIDMigration(new List<SourceTargetRange> { step0 });
             Assert.IsTrue(VerifyGroupIDMigration(step0.TargetFrom));
             editor.PerformColorIDMigration(new List<SourceTargetRange> { step2 });
@@ -90,69 +113,79 @@ namespace GDAPI.Tests.Application.Editors
             editor.PerformBlockIDMigration(new List<SourceTargetRange> { step1 });
             Assert.IsTrue(VerifyBlockIDMigration(step0.SourceFrom, step1.TargetFrom));
             // TODO: Add more test steps with multiple step ID migrations
-        }
 
-        private bool VerifyGroupIDMigration(int offset)
-        {
-            for (int i = 0; i < 5; i++)
-                if (normalBlocks[i].GetGroupID(0) != i + offset)
-                    return false;
-            for (int i = 0; i < 5; i++)
-                if (moveTriggers[i].TargetGroupID != i + offset)
-                    return false;
-            for (int i = 0; i < 5; i++)
-                if (instantCountTriggers[i].TargetGroupID != i + offset)
-                    return false;
-            for (int i = 0; i < 5; i++)
-                if (collisionTriggers[i].TargetGroupID != i + offset)
-                    return false;
-            return true;
-        }
-        private bool VerifyColorIDMigration(int offsetA, int offsetB)
-        {
-            for (int i = 0; i < 5; i++)
+            bool VerifyGroupIDMigration(int offset)
             {
-                if (normalBlocks[i].Color1ID != i + offsetA)
-                    return false;
-                if (normalBlocks[i].Color2ID != i + offsetB)
-                    return false;
+                for (int i = 0; i < 5; i++)
+                    if (normalBlocks[i].GetGroupID(0) != i + offset)
+                        return false;
+                for (int i = 0; i < 5; i++)
+                    if (moveTriggers[i].TargetGroupID != i + offset)
+                        return false;
+                for (int i = 0; i < 5; i++)
+                    if (instantCountTriggers[i].TargetGroupID != i + offset)
+                        return false;
+                for (int i = 0; i < 5; i++)
+                    if (collisionTriggers[i].TargetGroupID != i + offset)
+                        return false;
+                return true;
             }
-            for (int i = 0; i < 5; i++)
+            bool VerifyColorIDMigration(int offsetA, int offsetB)
             {
-                if (colorTriggers[i].TargetColorID != i + offsetA)
-                    return false;
-                if (colorTriggers[i].CopiedColorID != i + offsetB)
-                    return false;
+                for (int i = 0; i < 5; i++)
+                {
+                    if (normalBlocks[i].Color1ID != i + offsetA)
+                        return false;
+                    if (normalBlocks[i].Color2ID != i + offsetB)
+                        return false;
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    if (colorTriggers[i].TargetColorID != i + offsetA)
+                        return false;
+                    if (colorTriggers[i].CopiedColorID != i + offsetB)
+                        return false;
+                }
+                for (int i = 26; i <= 50; i++)
+                {
+                    int origin = i - 25;
+                    if (level.ColorChannels[i].CopiedColorID != i)
+                        return false;
+                    if (level.ColorChannels[i].ColorChannelID != i)
+                        return false;
+                    if (level.ColorChannels[origin].ColorChannelID != origin)
+                        return false;
+                }
+                return true;
             }
-            return true;
-        }
-        private bool VerifyItemIDMigration(int offset)
-        {
-            for (int i = 0; i < 5; i++)
-                if (pickupItems[i].TargetItemID != i + offset)
-                    return false;
-            for (int i = 0; i < 5; i++)
-                if (pickupTriggers[i].TargetItemID != i + offset)
-                    return false;
-            return true;
-        }
-        private bool VerifyBlockIDMigration(int offsetA, int offsetB)
-        {
-            for (int i = 0; i < 5; i++)
-                if (collisionBlocks[i].BlockID != i + offsetA)
-                    return false;
-            for (int i = 0; i < 5; i++)
+            bool VerifyItemIDMigration(int offset)
             {
-                if (collisionTriggers[i].PrimaryBlockID != i + offsetA)
-                    return false;
-                if (collisionTriggers[i].SecondaryBlockID != i + offsetB)
-                    return false;
+                for (int i = 0; i < 5; i++)
+                    if (pickupItems[i].TargetItemID != i + offset)
+                        return false;
+                for (int i = 0; i < 5; i++)
+                    if (pickupTriggers[i].TargetItemID != i + offset)
+                        return false;
+                return true;
             }
-            return true;
+            bool VerifyBlockIDMigration(int offsetA, int offsetB)
+            {
+                for (int i = 0; i < 5; i++)
+                    if (collisionBlocks[i].BlockID != i + offsetA)
+                        return false;
+                for (int i = 0; i < 5; i++)
+                {
+                    if (collisionTriggers[i].PrimaryBlockID != i + offsetA)
+                        return false;
+                    if (collisionTriggers[i].SecondaryBlockID != i + offsetB)
+                        return false;
+                }
+                return true;
+            }
         }
         #endregion
 
-        #region IDReallocation
+        #region ID Reallocation
         private static readonly int[] primaryTargetIDs = { 1, 2, 3, 4, 5 };
         private static readonly int[] secondaryTargetIDs = { 6, 7, 8, 9, 10 };
         private static readonly int[] primaryTargetIDsWithIgnored = { 30, 40, 1, 2, 3 };
@@ -161,22 +194,22 @@ namespace GDAPI.Tests.Application.Editors
         [Test]
         public void GroupIDReallocation()
         {
-            RunIDReallocationTest(AssertGroupIDReallocation, IDMigrationMode.Groups);
+            RunIDReallocationTest(IDMigrationMode.Groups, AssertGroupIDReallocation);
         }
         [Test]
         public void ColorIDReallocation()
         {
-            RunIDReallocationTest(AssertColorIDReallocation, IDMigrationMode.Colors);
+            RunIDReallocationTest(IDMigrationMode.Colors, AssertColorIDReallocation, AssertExtraColorIDReallication);
         }
         [Test]
         public void ItemIDReallocation()
         {
-            RunIDReallocationTest(AssertItemIDReallocation, IDMigrationMode.Items);
+            RunIDReallocationTest(IDMigrationMode.Items, AssertItemIDReallocation);
         }
         [Test]
         public void BlockIDReallocation()
         {
-            RunIDReallocationTest(AssertBlockIDReallocation, IDMigrationMode.Blocks);
+            RunIDReallocationTest(IDMigrationMode.Blocks, AssertBlockIDReallocation);
         }
 
         private void AssertGroupIDReallocation(int i, int id1, int id2)
@@ -206,11 +239,14 @@ namespace GDAPI.Tests.Application.Editors
             Assert.AreEqual(id2, collisionTriggers[i].SecondaryBlockID);
         }
 
-        private void RunIDReallocationTest(PostReallocationAssertion assertion, IDMigrationMode mode)
+        private void AssertExtraColorIDReallication(List<SourceTargetRange> steps, List<Range> ignoredRanges)
         {
-            InitializeObjects();
-            var level = new Level { LevelObjects = allObjects };
-            var editor = new Editor(level);
+
+        }
+
+        private void RunIDReallocationTest(IDMigrationMode mode, PostReallocationAssertion assertion, PostReallocationExtra extra = null)
+        {
+            ReinitializeLevelStuff();
 
             var steps = new List<SourceTargetRange>();
             for (int i = 4; i >= 0; i--)
@@ -227,10 +263,10 @@ namespace GDAPI.Tests.Application.Editors
                 int id2 = secondaryTargetIDs[i];
                 assertion(i, id1, id2);
             }
+            extra?.Invoke(steps, null);
 
             // Reset object IDs
-            InitializeObjects();
-            level.LevelObjects = allObjects;
+            ReinitializeLevelStuff();
             editor.PerformMigration(mode, steps);
 
             // Current ranges are 30, 40, 50, 60, 70 and 901, 902, 903, 904, 905
@@ -249,9 +285,11 @@ namespace GDAPI.Tests.Application.Editors
                 int id2 = secondaryTargetIDsWithIgnored[i];
                 assertion(i, id1, id2);
             }
+            extra?.Invoke(steps, ignoredRanges);
         }
 
         private delegate void PostReallocationAssertion(int i, int id1, int id2 = default);
+        private delegate void PostReallocationExtra(List<SourceTargetRange> steps, List<Range> ignoredRanges);
         #endregion
     }
 }
