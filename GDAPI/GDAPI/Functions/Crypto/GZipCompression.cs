66b3c75e7ac400dd48e8bf5ace1ecff00593cc9e
AlFas <AlFasGD@users.noreply.github.com>
2020-05-19 08:28:32 +0300

    Fix and improve compression/decompression code

--- GDAPI/GDAPI/Functions/Crypto/GZipCompression.cs
+++ GDAPI/GDAPI/Functions/Crypto/GZipCompression.cs
@@ -9,23 +9,26 @@ namespace GDAPI.Functions.Crypto
     {
         /// <summary>Compresses a <seealso cref="byte"/>[] and returns the compressed result.</summary>
         /// <param name="data">The <seealso cref="byte"/>[] of data to compress.</param>
-        public static byte[] Compress(byte[] data) => Compress(data, CompressionMode.Compress);
+        public static byte[] Compress(byte[] data)
+        {
+            using var uncompressedStream = new MemoryStream(data);
+            using var resultStream = new MemoryStream();
+            using var zipStream = new GZipStream(resultStream, CompressionMode.Compress);
+
+            uncompressedStream.CopyTo(zipStream);
+            zipStream.Close();
+            return resultStream.ToArray();
+        }
+
         /// <summary>Decompresses a <seealso cref="byte"/>[] and returns the decompressed result.</summary>
         /// <param name="data">The <seealso cref="byte"/>[] of data to decompress.</param>
-        public static byte[] Decompress(byte[] data) => Compress(data, CompressionMode.Decompress);
-
-        private static byte[] Compress(byte[] data, CompressionMode compressionMode)
+        public static byte[] Decompress(byte[] data)
         {
             using var compressedStream = new MemoryStream(data);
-            using var zipStream = new GZipStream(compressedStream, compressionMode);
             using var resultStream = new MemoryStream();
+            using var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress);
 
-            var buffer = new byte[4096];
-            int read;
-
-            while ((read = zipStream.Read(buffer, 0, buffer.Length)) > 0)
-                resultStream.Write(buffer, 0, read);
-
+            zipStream.CopyTo(resultStream);
             return resultStream.ToArray();
         }
     }
