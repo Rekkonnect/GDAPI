30aade4ff3456e3375fc4c767a4c31a553295a51
Alex <kalfas.alex@gmail.com>
2021-10-04 22:11:30 +0300

    Fix catastrophic bug

--- GDAPI/GDAPI/Objects/GeometryDash/GamesaveStrings/BaseGamesaveString.cs
+++ GDAPI/GDAPI/Objects/GeometryDash/GamesaveStrings/BaseGamesaveString.cs
@@ -10,7 +10,7 @@ namespace GDAPI.Objects.GeometryDash.GamesaveStrings
         public string RawString { get; set; }
 
         /// <summary>Gets the length of the raw string.</summary>
-        public int Length => RawString.Length;
+        public int Length => RawString?.Length ?? 0;
 
         /// <summary>Gets a value indicating whether the current gamesave string is decrypted or not.</summary>
         public bool IsDecrypted => !IsEncrypted;
@@ -19,6 +19,9 @@ namespace GDAPI.Objects.GeometryDash.GamesaveStrings
         {
             get
             {
+                if (IsEmpty)
+                    return true;
+
                 int checks = 0;
                 var samples = GetUnencryptedSamples();
                 for (int i = 0; i < samples.Length; i++)
@@ -27,6 +30,9 @@ namespace GDAPI.Objects.GeometryDash.GamesaveStrings
                 return checks != samples.Length;
             }
         }
+
+        /// <summary>Determines whether the string is empty or not. Prefer using this property over <seealso cref="RawString"/>, to avoid requiring a null check.</summary>
+        public bool IsEmpty => Length == 0;
 
         /// <summary>Initializes a new instance of the <seealso cref="BaseGamesaveString"/>.</summary>
         /// <param name="rawString">The raw string to contain in this instance.</param>
@@ -46,8 +52,9 @@ namespace GDAPI.Objects.GeometryDash.GamesaveStrings
                 RawString = decrypted;
             return isEncrypted;
         }
-        /// <summary>Returns the decrypted version of the gamesave after checking whether the gamesave is encrypted or not asynchronously. Returns a tuple containing the result of the operation.</summary>
+        /// <summary>Returns the decrypted version of the gamesave after checking whether the gamesave is encrypted or not asynchronously.</summary>
         /// <param name="overwriteLocalString">Determines whether to overwrite the locally stored raw string or not, replacing it with the decrypted version.</param>
+        /// <returns>A <seealso cref="DecryptionResult"/> containing the result of the decryption operation.</returns>
         public async Task<DecryptionResult> TryDecryptAsync(bool overwriteLocalString = false)
         {
             bool result = TryDecrypt(out string decrypted, overwriteLocalString);
@@ -68,8 +75,9 @@ namespace GDAPI.Objects.GeometryDash.GamesaveStrings
                 RawString = encrypted;
             return isDecrypted;
         }
-        /// <summary>Returns the encrypted version of the gamesave after checking whether the gamesave is encrypted or not asynchronously. Returns a tuple containing the result of the operation.</summary>
+        /// <summary>Returns the encrypted version of the gamesave after checking whether the gamesave is encrypted or not asynchronously.</summary>
         /// <param name="overwriteLocalString">Determines whether to overwrite the locally stored raw string or not, replacing it with the encrypted version.</param>
+        /// <returns>An <seealso cref="EncryptionResult"/> containing the result of the encryption operation.</returns>
         public async Task<EncryptionResult> TryEncryptAsync(bool overwriteLocalString = false)
         {
             bool result = TryEncrypt(out string encrypted, overwriteLocalString);
@@ -78,7 +86,7 @@ namespace GDAPI.Objects.GeometryDash.GamesaveStrings
 
         /// <summary>Decrypts the current raw string into its unencrypted form and returns the resulting string. The string must be encrypted; the operation will be performed in all occassions.</summary>
         protected abstract string Decrypt();
-        /// <summary>Encrypts the current raw string into its encrypted form and returns the resulting string. The string must be deencrypted; the operation will be performed in all occassions.</summary>
+        /// <summary>Encrypts the current raw string into its encrypted form and returns the resulting string. The string must be unencrypted; the operation will be performed in all occassions.</summary>
         protected abstract string Encrypt();
 
         /// <summary>Gets the unencrypted samples that determine whether the gamesave string is encrypted or not. They must be large parts of a typical gamesave string that will always be present in the save.</summary>
