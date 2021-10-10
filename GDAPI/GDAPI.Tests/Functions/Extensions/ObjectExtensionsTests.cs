befea5cd024bfec2037c765445227ad5b3e845f6
naoei <altenyt@gmail.com>
2021-10-10 15:44:56 -0400

    Add object extensions

---
+++ GDAPI/GDAPI.Tests/Functions/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using GDAPI.Functions.Extensions;
+using NUnit.Framework;
+
+namespace GDAPI.Tests.Functions.Extensions
+{
+    public class ObjectExtensionsTests
+    {
+        private readonly object testObject = new
+        {
+            field1 = true,
+            field2 = false
+        };
+        
+        [Test]
+        public void TestGetPropertiesName()
+        {
+            var properties = testObject.GetPropertiesName();
+            
+            Assert.AreEqual("field1", properties[0]);
+            Assert.AreEqual("field2", properties[1]);
+        }
+
+        [Test]
+        public void TestToDictionary()
+        {
+            var dict = testObject.ToDictionary();
+            
+            Assert.IsTrue(dict.ContainsKey("field1"), "Dictionary did not include \"field1\".");
+            Assert.IsTrue(dict.ContainsKey("field2"), "Dictionary did not include \"field2\".");
+            
+            Assert.AreEqual(true, dict["field1"]);
+            Assert.AreEqual(false, dict["field2"]);
+        }
+    }
+}
---
+++ GDAPI/GDAPI/Functions/Extensions/ObjectExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace GDAPI.Functions.Extensions
+{
+    /// <summary>Contains extension methods for objects.</summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>Gets property infos from an object.</summary>
+        /// <param name="data">The object to get the property infos from.</param>
+        /// <returns>An array of <see cref="PropertyInfo"/>.</returns>
+        public static PropertyInfo[] GetPropertyInfos(this object data) 
+            => data.GetType().GetProperties();
+
+        /// <summary>Gets all of the property names from an object.</summary>
+        /// <param name="data">The object to get the property names from.</param>
+        /// <returns>An array of string with the property names.</returns>
+        public static string[] GetPropertiesName(this object data) 
+            => data.GetPropertyInfos().Select(o => o.Name).ToArray();
+
+        /// <summary>Converts any object into a dictionary containing it's properties and values.</summary>
+        /// <param name="data">The object to convert into a dictionary</param>
+        public static Dictionary<string, object> ToDictionary(this object data)
+        {
+            var props = data.GetType().GetProperties();
+            return props.ToDictionary(prop => prop.Name, prop => prop.GetValue(data));
+        }
+    }
+}
